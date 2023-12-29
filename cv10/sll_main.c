8794171a047d7dd6401233fa905f6fbc2e8bb98a
Jorgen98 <juraj51112@gmail.com>
2023-12-29 12:28:20 +0100

    cv10 solution

---
+++ cv10/sll_main.c
@@ -0,0 +1,75 @@
+/**
+ * Kostra pro cviceni operaci nad jednosmerne vazanym seznamem.
+ */
+
+#include "sll.h"
+#include <stdio.h>
+
+ 
+int main()
+{
+    printf("list_ctor...\n");
+    List list = list_ctor();
+ 
+    printf("list_empty...\n");
+    printf("Seznam prazdny: %s\n", list_empty(&list) ? "ano" : "ne");
+ 
+    Item *item;
+ 
+    Object o1 = {42, "Honza"};
+    printf("item_ctor...\n");
+    item = item_ctor(o1);
+    printf("list_insert_first...\n");
+    list_insert_first(&list, item);
+ 
+    printf("Seznam prazdny: %s\n", list_empty(&list) ? "ano" : "ne");
+    printf("list_count...\n");
+    printf("Pocet prvku v seznamu: %d\n", list_count(&list));
+ 
+    Object o2 = {2, "Malem"};
+    item = item_ctor(o2);
+    printf("list_insert_first...\n");
+    list_insert_first(&list, item);
+ 
+    Object o3 = {0, "Kralem"};
+    item = item_ctor(o3);
+    printf("list_insert_first...\n");
+    list_insert_first(&list, item);
+ 
+    printf("Pocet prvku v seznamu: %d\n", list_count(&list));
+ 
+    printf("Odstraneni prvniho prvku ze seznamu \n");
+    list_delete_first(&list);
+    printf("Pocet prvku v seznamu: %d\n", list_count(&list));
+ 
+ 
+    // opetovne vlozeni objektu o1		
+    item = item_ctor(o1);
+    printf("list_insert_first...\n");
+    list_insert_first(&list, item);
+ 
+    printf("list_find_minid...\n");
+    item = list_find_minid(&list);
+    if (item != NULL) {
+        printf("Polozka s nejmensim identifikatorem: {%d, \"%s\"}\n",
+            item->data.id, item->data.name);
+	}
+	else
+        printf("Polozka s nejmensim identifikatorem nenalezena\n");
+ 
+    printf("list_find_name...\n");
+    char* name = "Honza";
+    item = list_find_name(&list, name);
+    if (item != NULL) {
+        printf("Polozka s daty %s nalezena\n", name);
+	} 
+	else
+        printf("Polozka s daty %s nenalezena.\n",name);
+ 
+    printf("list_dtor...\n");
+    list_dtor(&list);
+    printf("Seznam prazdny: %s\n", list_empty(&list) ? "ano" : "ne");
+ 
+    return 0;
+}
+
